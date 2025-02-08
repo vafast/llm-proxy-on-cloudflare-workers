72b27a550db4f405bb7830db6f7e200de0668674
blue.pen5805 <blue.pen5805@fastmail.com>
2025-02-08 06:57:47 +0000

    Update shuffle test with a larger array.

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -30,7 +30,11 @@ describe("getPathname", () => {
 
 describe("shuffle", () => {
   it("should shuffle the array", () => {
-    const array = [1, 2, 3, 4, 5];
+    const array = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    ];
     const result = shuffle(array);
     expect(result).not.toEqual(array); // It's possible to get the same array, but very unlikely
     expect(result.sort()).toEqual(array.sort()); // Ensure all elements are still present
